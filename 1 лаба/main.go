5a91abb68221b0031f1de02f4a20f9f2170d629d
Петр Опалюк <p_opaluk@mail.ru>
2026-09-17 21:35:09 +0300

    Петр Опалюк Лаб1

--- 1 лаба/main.go
+++ 1 лаба/main.go
@@ -74,3 +74,81 @@ func AddStudentsManually() {
 	students = append(students, student)
 	fmt.Printf("✅ Студент %s %s добавлен!\n", name, secondName)
 }
+
+// -----------------------------------------------------------------------------------------------------
+
+func SortStudentsByLessScore() {
+	sort.Slice(students, func(i, j int) bool {
+		return students[i].AvgScore < students[j].AvgScore
+	})
+}
+
+func SortStudentsByMoreScore() {
+	sort.Slice(students, func(i, j int) bool {
+		return students[i].AvgScore > students[j].AvgScore
+	})
+}
+
+func PrintStudents() {
+	if len(students) == 0 {
+		fmt.Println("❌ Список студентов пуст.")
+		return
+	}
+	fmt.Println("\n========== СПИСОК СТУДЕНТОВ ==========")
+	for i, student := range students {
+		fmt.Printf(
+			"\n№ %d\nИмя: %s %s %s\nВозраст: %d\nСредний балл: %.1f\nОтчислен: %t\n",
+			i+1,
+			student.Name,
+			student.SecondName,
+			student.LastName,
+			student.Age,
+			student.AvgScore,
+			student.Expelled)
+	}
+	fmt.Println("\n=======================================")
+}
+
+func main() {
+	for {
+		fmt.Println("\n========== МЕНЮ ==========")
+		fmt.Println("1. Добавить студента вручную")
+		fmt.Println("2. Добавить случайного студента")
+		fmt.Println("3. Добавить несколько случайных студентов")
+		fmt.Println("4. Показать всех студентов")
+		fmt.Println("5. Сортировать по возрастанию балла")
+		fmt.Println("6. Сортировать по убыванию балла")
+		fmt.Println("0. Выход")
+		fmt.Println("===========================")
+
+		var choice int
+		fmt.Print("Выберите действие: ")
+		fmt.Scan(&choice)
+		switch choice {
+		case 1:
+			AddStudentsManually()
+		case 2:
+			AddStudentRandom()
+		case 3:
+			var count int
+			fmt.Print("Сколько студентов добавить? ")
+			fmt.Scan(&count)
+			for i := 0; i < count; i++ {
+				AddStudentRandom()
+			}
+		case 4:
+			PrintStudents()
+		case 5:
+			SortStudentsByLessScore()
+			fmt.Println("✅ Студенты отсортированы по возрастанию балла.")
+		case 6:
+			SortStudentsByMoreScore()
+			fmt.Println("✅ Студенты отсортированы по убыванию балла.")
+		case 0:
+			fmt.Println("👋 Программа завершена.")
+			return
+		default:
+			fmt.Println("❌ Такого пункта меню нет.")
+		}
+	}
+}
